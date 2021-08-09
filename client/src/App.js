c63e2236d30210de58dd769d39db76093dd43f63
dasoml <somgrapik@gmail.com>
2021-08-09 21:09:03 +0900

    install cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import { async } from "rxjs";
 
 function App() {
+  const [bookmarks, setBookmarks] = useState(null);
+
+  useEffect(async () => {
+    const res = await axios.get("http://localhost:3001");
+    console.log(res);
+  }, []);
   return (
     <div className="App">
       <h1>Bookmark Archive app</h1>

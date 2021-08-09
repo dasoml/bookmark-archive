561f584ffaa2927af488a7d84c03ed51f1b3b653
dasoml <somgrapik@gmail.com>
2021-08-09 16:19:16 +0900

    add axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+function Welcome(props) {
+  return <h2>Hello, {props.name}</h2>;
+}
+
 function App() {
-  return <div className="App">Bookmark Archive app</div>;
+  return (
+    <div className="App">
+      <h1>Bookmark Archive app</h1>
+      <Welcome name="there"></Welcome>
+    </div>
+  );
 }
 
 export default App;
